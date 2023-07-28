683e27d47ffc32980e9570f86d2ba3911fd8fd47
Akash Shanmugaraj <65720968+akashShanmugraj@users.noreply.github.com>
2023-07-28 02:19:10 +0000

    final commit for 19Z205

--- 19Z205 - C Programming/working/sample.c
+++ 19Z205 - C Programming/working/sample.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
-
-void printelements(int **arraypointer, int size1, int size2) {
-    printf("%d")
-    for (int i = 0; i < size1; i++) {
-        for (int j = 0; j < size2; j++) {
-            printf("from numberarray = %d\n", );
-        }
-    }
-}
+#include <stdlib.h>
 
 int main() {
-    int numberarray[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
-    int *numberarraypointer = numberarray;
-    int **numberarraypointer2 = &numberarraypointer;
-    printelements(numberarraypointer2, 4, 3);
-    return 0;
+    FILE *fp = fopen("s.txt", "r");
+    if (fp == NULL) {
+        printf("Error opening file\n");
+        exit(1);
+    }
+
+    // find average of numbers stored in the file
+    int n, count = 0;
+    double sum = 0.0;
+    while (fgetc(fp) != EOF) {
+        while(fgetc(fp) != '\n') {
+            printf("%c", fgetc(fp));
+        }
+        printf("\n");
+    }
+
+
 }
