82bd195a2169d3d6ead2c0180df5136086e8535e
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-08-21 07:53:47 +0530

    Bubblesort and Homework for 21st August

--- 19Z302 - Data Structures and Algorithms/README.md
+++
@@ -1,4 +0,0 @@
-# 19Z302 - Data Structures and Algorithms
-
-Taught by **Dr Lovelyn Rose S, Associate Professor, PSG College of Technology**.
-Notes drafted and maintained by Akash Shanmugaraj, PSGCT'26.
---
+++ 19Z302 - Data Structures and Algorithms/Sorting Algorithms/Bubble Sort/Testcase Generation/nearlysorted.py
@@ -0,0 +1,34 @@
+# this file generate nearly sorted test cases for predefiled ranges
+import random
+
+PRE_HALF_LIST_1 = list(range(0, 100))
+SHF_RANGE_1 = list(range(100,151))
+random.shuffle(SHF_RANGE_1)
+POST_HALF_LIST_1 = list(range(151, 1001))
+
+PRE_HALF_LIST_2 = list(range(0, 860))
+SHF_RANGE_2 = list(range(860, 901))
+random.shuffle(SHF_RANGE_2)
+POST_HALF_LIST_2 = list(range(901, 1001))
+
+PRE_HALF_LIST_3 = list(range(0, 430))
+SHF_RANGE_3 = list(range(430, 513))
+random.shuffle(SHF_RANGE_3)
+POST_HALF_LIST_3 = list(range(513, 1001))
+
+
+FIRST_STRING = '1000 ' + ' '.join(str(x) for x in PRE_HALF_LIST_1) + ' ' + ' '.join(str(x) for x in SHF_RANGE_1) + ' ' + ' '.join(str(x) for x in POST_HALF_LIST_1)
+SECOND_STRING = '1000 ' + ' '.join(str(x) for x in PRE_HALF_LIST_2) + ' ' + ' '.join(str(x) for x in SHF_RANGE_2) + ' ' + ' '.join(str(x) for x in POST_HALF_LIST_2)
+THIRD_STRING = '1000 ' + ' '.join(str(x) for x in PRE_HALF_LIST_3) + ' ' + ' '.join(str(x) for x in SHF_RANGE_3) + ' ' + ' '.join(str(x) for x in POST_HALF_LIST_3)
+
+file1 = open("nearlysorted1.txt", "w")
+file2 = open("nearlysorted2.txt", "w")
+file3 = open("nearlysorted3.txt", "w")
+
+file1.write(FIRST_STRING + "\n")
+file2.write(SECOND_STRING + "\n")
+file3.write(THIRD_STRING + "\n")
+
+file1.close()
+file2.close()
+file3.close()
