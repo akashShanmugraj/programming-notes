5164e19643628bc1b9ec8747b8db8ef9a684505a
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-11-21 15:54:02 +0530

    Final code for left and right rotation

--- 19Z302 - Data Structures and Algorithms/AVL Trees/main.c
+++ 19Z302 - Data Structures and Algorithms/AVL Trees/main.c
@@ -1,108 +1,148 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Step 1: Define a struct for the tree node
-struct node {
+struct node
+{
     int data;
-    struct node* left;
-    struct node* right;
+    struct node *left;
+    struct node *right;
 };
 
-// Step 2: Create a function to create a new node
-struct node* newNode(int data) {
-    struct node* node = (struct node*)malloc(sizeof(struct node));
+struct node *newNode(int data)
+{
+    struct node *node = (struct node *)malloc(sizeof(struct node));
     node->data = data;
     node->left = NULL;
     node->right = NULL;
-    return(node);
+    return (node);
 }
 
-// Step 3: Create a function to print the tree
-void printTree(struct node * rootnode, int level) {
-    if (rootnode == NULL) {
+void printTree(struct node *root, int level)
+{
+    if (root == NULL)
+    {
         return;
     }
-    printTree(rootnode -> right, level + 1);
-    for (int i = 0; i < level; i++) {
-        printf("    ");
+    printTree(root->right, level + 1);
+    for (int i = 0; i < level; i++)
+    {
+        printf("|\t");
     }
-    printf("%d", rootnode -> data);
-    printTree(rootnode -> left, level + 1);
+    printf("|------>%d\n", root->data);
+    printTree(root->left, level + 1);
 }
 
-int getMaxHeight(struct node * rootnode){
-    if (rootnode == NULL){
+int getMaxHeight(struct node *rootnode)
+{
+    if (rootnode == NULL)
+    {
         return 0;
     }
-    else {
+    else
+    {
         int leftHeight = getMaxHeight(rootnode->left);
         int rightHeight = getMaxHeight(rootnode->right);
-        
-        if (leftHeight > rightHeight) {
-            return(leftHeight + 1);
+
+        if (leftHeight > rightHeight)
+        {
+            return (leftHeight + 1);
         }
-        else {
-            return(rightHeight + 1);
+        else
+        {
+            return (rightHeight + 1);
         }
     }
 }
 
-void getBalanceFactor(struct node * rootnode){
-    int balancefactor = getMaxHeight(rootnode->left)-getMaxHeight(rootnode->right);
+void getBalanceFactor(struct node *rootnode)
+{
+    int balancefactor = getMaxHeight(rootnode->left) - getMaxHeight(rootnode->right);
 
     printf("CUR %d \t BF %d \t", rootnode->data, balancefactor);
 
-    if (balancefactor < -1 || balancefactor > 1){
+    if (balancefactor < -1 || balancefactor > 1)
+    {
         printf("IMBALANCE!\n");
-    } else {
+    }
+    else
+    {
         printf("\n");
     }
 
-    if (rootnode->left != NULL){
+    if (rootnode->left != NULL)
+    {
         getBalanceFactor(rootnode->left);
     }
-    
-    if (rootnode->right != NULL){
+
+    if (rootnode->right != NULL)
+    {
         getBalanceFactor(rootnode->right);
     }
 }
 
-struct node * rotateRight(struct node * pivotnode, struct node * parentnode){
-    if (pivotnode->right == NULL){
-        pivotnode->right = parentnode;
-    } else {
-        struct node * pivotrightnode = pivotnode->right;
-        pivotnode->right = parentnode;
-        parentnode->left = pivotrightnode;
+struct node *rotateRight(struct node *parentnode)
+{
+    struct node *pivotnode = parentnode->left;
+    if (pivotnode == NULL || parentnode == NULL)
+    {
+        return NULL;
     }
+
+    parentnode->left = pivotnode->right;
+    pivotnode->right = parentnode;
 
     return pivotnode;
 }
-int main() {
-    // Step 4: Create 3 nodes and link them to form a left-skewed tree
-    struct node *root = newNode(20);
-    root->left = newNode(10);
-    root->left->left = newNode(5);
 
+struct node *rotateLeft(struct node *parentnode)
+{
+    struct node *pivotnode = parentnode->right;
+    if (pivotnode == NULL || parentnode == NULL)
+    {
+        return NULL;
+    }
 
-    // Step 5: Call the printTree function to print the tree
-    printTree(root, 0);
+    parentnode->right = pivotnode->left;
+    pivotnode->left = parentnode;
 
-    int * leftcounterpointer = 0;
-    int * rightcounterpointer = 0;
+    return pivotnode;
+}
 
-    // printf("MAX HEIGHT %d\n", getMaxHeight(root));
+int main()
+{
+    printf("ROTATE RIGHT:\n");
+    struct node *leftskewtree = newNode(20);
+    leftskewtree->left = newNode(10);
+    leftskewtree->left->left = newNode(5);
 
-    // getBalanceFactor(root);
+    printTree(leftskewtree, 0);
+
+    printf("MAX HEIGHT %d\n", getMaxHeight(leftskewtree));
+
+    getBalanceFactor(leftskewtree);
 
     printf("\n");
 
-    root = rotateRight(root->left, root);
-    printTree(root, 0);
+    leftskewtree = rotateRight(leftskewtree);
+    printTree(leftskewtree, 0);
 
-    printf("RT-VAL %d\n", root->data);
-    printf("RT-LVAL %d\n", root->left->data);
-    printf("RT-RVAL %d\n", root->right->data);
+    printf("\n\n\n");
+
+    printf("ROTATE LEFT: \n");
+    struct node *rightskewtree = newNode(20);
+    rightskewtree->right = newNode(10);
+    rightskewtree->right->right = newNode(5);
+
+    printTree(rightskewtree, 0);
+
+    printf("MAX HEIGHT %d\n", getMaxHeight(rightskewtree));
+
+    getBalanceFactor(rightskewtree);
+
+    printf("\n");
+
+    rightskewtree = rotateLeft(rightskewtree);
+    printTree(rightskewtree, 0);
 
     return 0;
 }
