958043714441c8671d55983be421d84a1cf48625
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-12-10 22:10:38 +0530

    Added code for graphs

---
+++ 19Z302 - Data Structures and Algorithms/Graphs/main.c
@@ -0,0 +1,84 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure to represent a node in the adjacency list
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Structure to represent the graph
+struct Graph {
+    int vertices;
+    struct Node** adjacencyList;
+};
+
+// Function to create a new node
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with a given number of vertices
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->vertices = vertices;
+
+    // Create an array of adjacency lists
+    graph->adjacencyList = (struct Node**)malloc(vertices * sizeof(struct Node*));
+
+    // Initialize each adjacency list as empty
+    for (int i = 0; i < vertices; ++i) {
+        graph->adjacencyList[i] = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add an edge to the graph
+void addEdge(struct Graph* graph, int src, int dest) {
+    // Add an edge from src to dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->adjacencyList[src];
+    graph->adjacencyList[src] = newNode;
+
+    // For undirected graphs, add an edge from dest to src as well
+    newNode = createNode(src);
+    newNode->next = graph->adjacencyList[dest];
+    graph->adjacencyList[dest] = newNode;
+}
+
+// Function to print the adjacency list representation of the graph
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->vertices; ++i) {
+        struct Node* current = graph->adjacencyList[i];
+        printf("Adjacency list of vertex %d: ", i);
+        while (current) {
+            printf("%d -> ", current->data);
+            current = current->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+// Example usage
+int main() {
+    int vertices = 5;
+    struct Graph* graph = createGraph(vertices);
+
+    printGraph(graph);
+
+    addEdge(graph, 0, 1);
+    addEdge(graph, 0, 4);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 1, 4);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+
+    printGraph(graph);
+
+    return 0;
+}
