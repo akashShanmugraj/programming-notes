4adcbdf22deed0e81ba1bda7c646794d5cfa527e
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-12-14 19:29:03 +0530

    boilerplate levelorder traversal code for srlize/d-srlize

--- 19Z310 - Data Structures Laboratory/Final Report/main.py
+++ 19Z310 - Data Structures Laboratory/Final Report/main.py
@@ -26,6 +26,25 @@ def level_order_traversal(root):
 
     return result
 
+# Insert from an array of values
+def insert_level_order(arr, root, i, n):
+    if i < n:
+        temp = TreeNode(arr[i])
+        root = temp
+
+        root.left = insert_level_order(arr, root.left, 2 * i + 1, n)
+        root.right = insert_level_order(arr, root.right, 2 * i + 2, n)
+
+    return root
+
+def print_tree(root, level=0):
+    if root is None:
+        return
+
+    print_tree(root.right, level + 1)
+    print('    ' * level + '->', root.val)
+    print_tree(root.left, level + 1)
+
 # Example usage:
 if __name__ == "__main__":
     # Creating a binary tree
@@ -40,3 +59,9 @@ if __name__ == "__main__":
     # Perform level order traversal
     result = level_order_traversal(root)
     print("Level Order Traversal:", result)
+
+    # Insert from an array of values
+    arr = result
+    root = None
+    root = insert_level_order(arr, root, 0, len(arr))
+    print_tree(root)
