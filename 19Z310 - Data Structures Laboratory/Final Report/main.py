eafc5f7feb2570bfe6a99784ecf9d61b75a89448
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2023-12-14 19:30:17 +0530

    Leetcode level refractor

--- 19Z310 - Data Structures Laboratory/Final Report/main.py
+++ 19Z310 - Data Structures Laboratory/Final Report/main.py
@@ -1,67 +1,61 @@
 from collections import deque
 
 class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
-        self.val = val
-        self.left = left
-        self.right = right
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
-def level_order_traversal(root):
-    if not root:
-        return []
+class Codec:
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        if not root:
+            return "[]"
+        
+        queue = deque([root])
+        result = []
+        
+        while queue:
+            node = queue.popleft()
+            if node:
+                result.append(str(node.val))
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                result.append('null')
+        
+        return '[' + ','.join(result) + ']'
 
-    result = []
-    queue = deque([root])
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        if data == "[]":
+            return None
+        
+        nodes, i = data[1:-1].split(','), 1
+        root = TreeNode(int(nodes[0]))
+        queue = deque([root])
+        
+        while queue:
+            node = queue.popleft()
+            if nodes[i] != 'null':
+                node.left = TreeNode(int(nodes[i]))
+                queue.append(node.left)
+            i += 1
+            if nodes[i] != 'null':
+                node.right = TreeNode(int(nodes[i]))
+                queue.append(node.right)
+            i += 1
+        
+        return root
 
-    print("Queue:", dir(queue))
-
-    while queue:
-        current_node = queue.popleft()
-        result.append(current_node.val)
-
-        if current_node.left:
-            queue.append(current_node.left)
-        if current_node.right:
-            queue.append(current_node.right)
-
-    return result
-
-# Insert from an array of values
-def insert_level_order(arr, root, i, n):
-    if i < n:
-        temp = TreeNode(arr[i])
-        root = temp
-
-        root.left = insert_level_order(arr, root.left, 2 * i + 1, n)
-        root.right = insert_level_order(arr, root.right, 2 * i + 2, n)
-
-    return root
-
-def print_tree(root, level=0):
-    if root is None:
-        return
-
-    print_tree(root.right, level + 1)
-    print('    ' * level + '->', root.val)
-    print_tree(root.left, level + 1)
-
-# Example usage:
-if __name__ == "__main__":
-    # Creating a binary tree
-    root = TreeNode(10)
-    root.left = TreeNode(7)
-    root.right = TreeNode(15)
-    root.left.left = TreeNode(5)
-    root.left.right = TreeNode(8)
-    root.right.left = TreeNode(12)
-    root.right.right = TreeNode(20)
-
-    # Perform level order traversal
-    result = level_order_traversal(root)
-    print("Level Order Traversal:", result)
-
-    # Insert from an array of values
-    arr = result
-    root = None
-    root = insert_level_order(arr, root, 0, len(arr))
-    print_tree(root)
+# Your Codec object will be instantiated and called as such:
+# codec = Codec()
+# codec.deserialize(codec.serialize(root))
