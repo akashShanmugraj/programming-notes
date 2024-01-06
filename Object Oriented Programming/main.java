abf7268be2723c2addf8430b99a72a0f37071359
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-01-06 12:40:04 +0530

    final commit for 19Z305

--- Object Oriented Programming/main.java
+++ Object Oriented Programming/main.java
@@ -1,69 +1,25 @@
 import java.util.*;
 
-public class Main {
-    public static <T extends Comparable<T>> T returnelement(T int1, T int2, T int3) {
-        T max = int1;
-        if (int2.compareTo(max) > 0) {
-            max = int2;
-        }
-        if (int3.compareTo(max) > 0) {
-            max = int3;
-        }
-        return max;
-    }
+public class main {
     public static void main(String[] args) {
-        System.out.println(returnelement(12,3,14));
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        list.add(1);
+        list.add(10);
+        list.add(20);
+        list.add(30);
+        list.add(40);
+        list.add(50);
+        list.add(60);
+        list.add(70);
+        list.add(80);
+        list.add(90);
+        list.add(100);
 
-        ArrayList al1 = new ArrayList();
-        al1.add(100);
-        al1.add("AADS");
-        al1.get(1);
+        list.remove(10);
 
-        System.out.println(al1);
+        list.forEach( (number) -> {
+            System.out.println(number);
+        });
 
-        ArrayList<Integer> integerarraylist = new ArrayList<Integer>();
-        integerarraylist.add(100);
-        integerarraylist.add(200);
-        integerarraylist.add(1);
-
-        System.out.println(integerarraylist);
-
-        integerarraylist.remove(1);
-
-        System.out.println(integerarraylist);
-
-        integerarraylist.remove(1);
-
-        System.out.println(integerarraylist);
-
-        System.out.println("Linked List");
-        LinkedList linkedlist1 = new LinkedList();
-
-        linkedlist1.add(100);
-        linkedlist1.add("one");
-
-        linkedlist1.addAll(integerarraylist);
-
-        System.out.println(linkedlist1);
-
-        System.out.println("PRQ");
-
-        PriorityQueue<Integer> prqueue = new PriorityQueue<Integer>();
-
-        prqueue.offer(50);
-        prqueue.offer(25);
-        prqueue.offer(75);
-
-        Object[] array = prqueue.toArray();
-        for (Object element : array) {
-            System.out.println(element);
-        }
-
-        for (Object node : prqueue) {
-            System.out.println("P"+node);
-        }
     }
-
-    
-
 }
