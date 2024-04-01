7c1b637175bdd69c4ba2bc92200ea2c9f93bc8bd
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-04-01 11:20:14 +0530

    Completed Exercise 6

--- 19Z411 - Database Management Systems Laboratory /lab6.sql
+++ 19Z411 - Database Management Systems Laboratory /lab6.sql
@@ -1,3 +1,55 @@
+SELECT * FROM member WHERE member_id IN (SELECT t.member_id FROM transaction t WHERE t.book_id=’B00009’);
+
+SELECT m.fname, m.lname, area FROM member m
+WHERE member_id IN (
+    SELECT member_id
+    FROM transaction
+    WHERE t_id = 't06'
+);
+
+SELECT fname, lname, phone_no
+FROM member
+WHERE member_id IN (
+    SELECT member_id
+    FROM transaction
+    WHERE issue_date < TO_DATE('01-AUG-19', 'DD-MON-YY')
+);
+
+SELECT title
+FROM books
+WHERE book_id IN (
+    SELECT book_id
+    FROM transaction
+    WHERE member_id IN (
+        SELECT member_id
+        FROM member
+        WHERE lname IN ('George', 'Basu')
+    )
+);
+
+SELECT type, book_id
+FROM books
+WHERE book_id IN (
+    SELECT book_id
+    FROM transaction
+    WHERE member_id IN ('M01', 'M06')
+);
+
+SELECT member_id
+FROM transaction
+WHERE book_id IN (
+    SELECT book_id
+    FROM books
+    WHERE author = 'R P Jain'
+);
+
+SELECT lname, fname
+FROM member
+WHERE member_id IN (
+    SELECT member_id
+    FROM transaction WHERE issue_date IS NOT NULL
+);
+
 UPDATE member SET phone_no = 7865466389 WHERE lname = 'Sneha';
 UPDATE transaction SET issue_date = TO_DATE('24-07-93', 'DD-MM-YY') WHERE member_id = 'M01';
 UPDATE books SET price = 1250 WHERE title = 'Data Structures Using C And C++';
