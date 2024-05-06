4cf2e7dd637bb21dfcd668cc171e388326e2690d
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-05-06 14:45:44 +0530

    removed tracing print statements

--- 19Z402 - Design and Analysis of Algorithms/Backtracking/simplefourqueens.py
+++ 19Z402 - Design and Analysis of Algorithms/Backtracking/simplefourqueens.py
@@ -1,4 +1,3 @@
-import time
 def issafe(qlist, xpos, ypos):
     for qxpos, qypos in qlist:
         if qxpos == xpos or qypos == ypos:
@@ -7,39 +6,23 @@ def issafe(qlist, xpos, ypos):
             return False
     return True
 
-def placequeens(grid, qlist, qcount,c):
+def placequeens(grid, qlist, qcount):
     if qcount == len(grid[0]):
-        print("gridCOMP", "\n\n")
+        print(grid, "\n\n")
         return
     allsafecells = []
     for cell in range(len(grid[0])):
         if issafe(qlist, qcount, cell):
             allsafecells.append([qcount, cell])
-    print("\t"*c[0],"ASFC", allsafecells)
-    
+
     for cell in allsafecells:
-        c[0] += 1
-        print("\t"*(c[0]-1),"PC(grid, ql, qc)")
         qlist.append(cell)
         grid[cell[0]][cell[1]] = f"Q{qcount}"
-        print("\t"*c[0],"PLACE", cell)
-        print("\t"*c[0],"QL:", qlist)
-        placequeens(grid, qlist, qcount + 1,c)
+        placequeens(grid, qlist, qcount + 1)
         grid[cell[0]][cell[1]] = 0
         qlist.pop()
-        c[0] -= 1
-        print("\t"*c[0],"REMOVE", cell)
-        print("\t"*c[0],"QLIST:", qlist)
 
 
-board = [
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-    [0, 0, 0, 0], 
-]
+board = [[0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]
 queenslist = []
-t1 = time.time()
-placequeens(board, queenslist, 0, [0])
-t2 = time.time()
-print(t2-t1)
+placequeens(board, queenslist, 0)
