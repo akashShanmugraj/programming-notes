1d912fabfc6e5cd183934877eb6e80ac16a9a2dd
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-08-12 11:55:26 +0530

    fibonacci def works

--- 19Z511 - Microprocessor and Interfacing Laboratory/Classroom Programs/fibonacci.s
+++ 19Z511 - Microprocessor and Interfacing Laboratory/Classroom Programs/fibonacci.s
@@ -1,26 +1,28 @@
 finalhalt:
-HLT
+    HLT
 
 update:
-MOV WORD [SI],DX
-ADD SI, 2
-JMP chpt1
+    MOV WORD [SI], DX
+    MOV BX,DX
+    ADD SI, 2
+    JMP chpt1
 
 start:
-MOV BYTE [0x1FFF],0x8 ; limit for fibonacci generation
-MOV WORD [0x2000],0x0 ; first number
-MOV WORD [0x2002],0x1 ; second number
+    MOV BYTE [0x1FFF], 0x8 ; limit for fibonacci generation
+    MOV WORD [0x2000], 0x0 ; first number
+    MOV WORD [0x2002], 0x1 ; second number
 
-MOV SI,0x2004
-MOV CL,BYTE [0x1FFF]
-MOV AX,WORD [0x2000]
-MOV BX,WORD [0x2002]
+    MOV SI, 0x2004
+    MOV CL, BYTE [0x1FFF]
+    MOV AX, WORD [0x2000]
+    MOV BX, WORD [0x2002]
 
 chpt1:
-CMP CL,0
-JZ finalhalt
+    CMP CL, 0
+    JZ finalhalt
 
-DEC CL
-MOV DX, AX
-ADD DX, BX
-MOV AX, BX
+    DEC CL
+    MOV DX, AX
+    ADD DX, BX
+    MOV AX, BX
+    JMP update
