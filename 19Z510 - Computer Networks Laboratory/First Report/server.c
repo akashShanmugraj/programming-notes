eca6787eec0b9977294a885317b17f9f5650f487
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-08-13 20:19:54 +0530

    correct server template

--- 19Z510 - Computer Networks Laboratory/First Report/server.c
+++ 19Z510 - Computer Networks Laboratory/First Report/server.c
@@ -12,35 +12,39 @@
 int main() {
 
   // Variables and structures
-  int client_fd;
-  struct sockaddr_in server_addr;
+  int server_fd, client_fd;
+  struct sockaddr_in server_addr, client_addr;
+  socklen_t addr_size;
   char buffer[1024];
 
-  // Client socket
-  client_fd = socket(AF_INET, SOCK_STREAM, 0);
+  // Server socket
+  server_fd = socket(AF_INET, SOCK_STREAM, 0);
 
   server_addr.sin_family = AF_INET;
   server_addr.sin_port = htons(PORT);
   server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
 
-  // Connect to the server
-  if (connect(client_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) == -1) {
-    printf("Connection failed\n");
-    return -1;
+  bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  listen(server_fd, 5);
+  printf("[LISTENING] Port Number: %d\n", PORT);
+
+  while (1) {
+    client_fd = accept(server_fd, (struct sockaddr *)&client_addr, &addr_size);
+    printf("[CONNECTED] New Connection\n");
+
+    strcpy(buffer, "Hello, This is a test message");
+    send(client_fd, buffer, strlen(buffer), 0);
+
+    memset(buffer, '\0', sizeof(buffer));
+    recv(client_fd, buffer, 1024, 0);
+    printf("[CLIENT] %s\n", buffer);
+
+    close(client_fd);
+    printf("[DISCONNECTED] Connection closed\n");
+
   }
-  printf("[CONNECTED] Connected to the server\n");
 
-  // Receive message from the server
-  recv(client_fd, buffer, 1024, 0);
-  printf("[SERVER] %s\n", buffer);
-
-  // Send message to the server
-  strcpy(buffer, "Hello, Server. This is the client.");
-  send(client_fd, buffer, strlen(buffer), 0);
-
-  // Close the socket
-  close(client_fd);
-  printf("[DISCONNECTED] Connection closed\n");
+  close(server_fd);
 
   return 0;
 }
