62de2f95bd234efacc0ad17e4a9d4ee8819d2421
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-08-20 11:28:00 +0530

    Finds Missing Number in given input

--- 19Z511 - Microprocessor and Interfacing Laboratory/Classroom Programs/findmissingnumberap.s
+++ 19Z511 - Microprocessor and Interfacing Laboratory/Classroom Programs/findmissingnumberap.s
@@ -1,13 +1,29 @@
+finalupdate:
+    MOV WORD [0x5000], AX
+    HLT
+
 start:
     MOV BYTE [0x2000],0x4 ; a-value in AP
     MOV BYTE [0x2002],0x2 ; l-value in AP
+
+    MOV BYTE [0x4000],0x4
+    MOV BYTE [0x4002],0x6
+    MOV BYTE [0x4004],0x8
+    MOV BYTE [0x4006],0xB
 
     MOV AX, WORD [0x2000]
     MOV CX, WORD [0x2002]
 
     MOV SI, 0x3000
+    MOV DI, 0x4000
 
 aplabel:
-    MOV [SI], AX
+    MOV WORD [SI], AX
+    MOV DX, WORD [DI]
+    CMP AX, DX
+    JNE finalupdate
     ADD SI, 2
+    ADD DI, 2
     ADD AX, CX
+    JMP aplabel
+
