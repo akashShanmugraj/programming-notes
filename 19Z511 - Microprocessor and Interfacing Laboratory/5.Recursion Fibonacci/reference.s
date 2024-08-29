80bddc123664be68323543f3d63e12609e1c966e
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-08-29 20:56:21 +0530

    iterative fibonacci with addr checkpoints

--- 19Z511 - Microprocessor and Interfacing Laboratory/5.Recursion Fibonacci/reference.s
+++ 19Z511 - Microprocessor and Interfacing Laboratory/5.Recursion Fibonacci/reference.s
@@ -1,25 +1,25 @@
-finalhalt:
+finalhalt: 1000
     HLT
 
-update:
+update: 1001
     CMP DX, CX
     JG finalhalt
     MOV WORD [SI], DX
     MOV BX,DX
     ADD SI, 2
-    JMP chpt1
+    JMP chpt1 100D
 
 start:
-    MOV BYTE [0x1FFF], 0x8 ; limit for fibonacci generation
+    MOV BYTE [0x1FFF], 0x8 ; limit for fibonacci generation 1
     MOV WORD [0x2000], 0x0 ; first number
     MOV WORD [0x2002], 0x1 ; second number
 
-    MOV SI, 0x2004
-    MOV CL, BYTE [0x1FFF]
-    MOV AX, WORD [0x2000]
-    MOV BX, WORD [0x2002]
+    MOV SI, 0x2004 1010
+    MOV CL, 0xe
+    MOV AX, 0x0
+    MOV BX, 0x1
 
-chpt1:
+chpt1: 101f
     MOV DX, AX
     ADD DX, BX
     MOV AX, BX
