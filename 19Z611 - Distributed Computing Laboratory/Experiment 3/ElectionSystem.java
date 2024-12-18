192ff8c39c4d03f15c30545469a19ab0f99be2d9
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-12-18 19:27:37 +0530

    added bully election code and ideal output

---
+++ 19Z611 - Distributed Computing Laboratory/Experiment 3/ElectionSystem.java
@@ -0,0 +1,147 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class ElectionSystem {
+
+    class Process {
+        int processid;
+        int priority;
+        boolean isDown;
+        boolean isCoordinator;
+        int timeout;
+
+        public Process(int id, int priority) {
+            this.processid = id;
+            this.priority = priority;
+            this.isDown = false;
+            this.isCoordinator = false;
+            this.timeout = 0;
+        }
+
+        public int reducetimeout() {
+            if (!this.isDown) {
+                return 1;
+            }
+
+            this.timeout -= 1;
+            if (this.timeout == 0) {
+                this.isDown = false;
+                return 0; 
+            }
+
+            return 1;
+        }
+
+        @Override
+        public String toString() {
+            return "Process{id=" + processid + ", prior=" + priority + ", down=" + isDown + ", coord=" + isCoordinator + "}";
+        }
+    }
+
+    public Process electCoordinator(List<Process> processList, int electType) {
+        int maxpriority = processList.get(0).priority;
+        Process maxvalprocess = processList.get(0);
+
+        for (Process processiter : processList) {
+            if (processiter.priority > maxpriority && !processiter.isDown) {
+            maxpriority = processiter.priority;
+                maxvalprocess = processiter;
+            }
+        }
+
+        if (electType == 1) {
+            System.out.println("Elected " + maxvalprocess.processid + " with priority " + maxvalprocess.priority + ".");
+        } else if (electType == 2) {
+            System.out.println("Relected " + maxvalprocess.processid + " with priority " + maxvalprocess.priority + ".");
+        }
+
+        return maxvalprocess;
+    }
+
+    public List<Process> randomprocesses(int len) {
+        List<Process> processList = new ArrayList<>();
+
+        Random rh = new Random();
+
+        for (int iter = 0; iter < len; iter++) {
+            int id = rh.nextInt(1000);         
+            int priority = rh.nextInt(100);  
+            processList.add(new Process(id, priority));
+        }
+
+        return processList;
+    }
+
+    public List<Process> serialprocesses(int len) {
+        List<Process> processList = new ArrayList<>();
+
+
+        for (int iter = 0; iter < len; iter++) {
+            int id = iter;
+            int priority = iter;
+            processList.add(new Process(id, priority));
+        }
+
+        return processList;
+    }
+
+
+    public boolean shouldKill() {
+        Random rm = new Random();
+        return rm.nextInt(100) < 30;
+    }
+
+    public void killRandom(List<Process> processeslist, int processescount) {
+        Random rm = new Random();
+        int killIndex = rm.nextInt(processescount);
+        processeslist.get(killIndex).isDown = true;
+        processeslist.get(killIndex).isCoordinator = false;
+        processeslist.get(killIndex).timeout = 5;
+        System.out.println("Process " + processeslist.get(killIndex).processid + " with priority " + processeslist.get(killIndex).priority + " was killed.");
+    }
+
+    public void killCoordinator(Process coordinatorProcess) {
+        coordinatorProcess.isCoordinator = false;
+        coordinatorProcess.isDown = true;
+        coordinatorProcess.timeout = 5;
+        System.out.println("Process " + coordinatorProcess.processid + " with priority " + coordinatorProcess.priority + " was killed.");
+    }
+
+
+    public static void main(String[] args) {
+        ElectionSystem electionsystem = new ElectionSystem();
+        int totalNumberofProcesses = 5;
+        // List<Process> processes = electionsystem.randomprocesses(totalNumberofProcesses);
+        List<Process> processes = electionsystem.serialprocesses(totalNumberofProcesses);
+
+        for (Process process : processes) {
+            System.out.println(process);
+        }
+
+        Process coordinatorreference = electionsystem.electCoordinator(processes, 1);
+
+        while (true) {
+            System.out.println("Coordinator is " + coordinatorreference.processid + " with priority " + coordinatorreference.priority);
+
+            if (electionsystem.shouldKill()) {
+                // electionsystem.killRandom(processes, totalNumberofProcesses);
+                electionsystem.killCoordinator(coordinatorreference);
+                coordinatorreference = electionsystem.electCoordinator(processes, 2);
+            }
+
+            for (Process process : processes) {
+                int response = process.reducetimeout();
+                if (response == 0) {
+                    coordinatorreference = electionsystem.electCoordinator(processes, 2);
+                }
+            }
+
+            try {
+                Thread.sleep(1000); // Delay for 1 second
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
