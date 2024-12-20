5889b6ef38409515faa76ac3e1cbd326c7a3e9f8
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-12-20 22:49:40 +0530

    added some more abstract functions

--- 19Z611 - Distributed Computing Laboratory/Experiment 3/RPC Election/RPCProcessInterface.java
+++ 19Z611 - Distributed Computing Laboratory/Experiment 3/RPC Election/RPCProcessInterface.java
@@ -4,11 +4,16 @@ import java.util.List;
 
 public interface RPCProcessInterface extends Remote {
     void addProcess(int processID, int priority, int port) throws RemoteException;
-    List<RPCProcessInterface> getProcesses() throws RemoteException;
+    List<String> getProcesses() throws RemoteException;
     void setProcesses(List<RPCProcessInterface> newprocesslist) throws RemoteException;
     int reducetimeout() throws RemoteException;
     void randomlyKillYourself() throws RemoteException;
     boolean isDown() throws RemoteException;
     int getPriority() throws RemoteException;
     RPCProcessInterface BullyElector() throws RemoteException;
+    void setCoordinator(RPCProcessInterface newCoordinator) throws RemoteException;
+    int getProcessID() throws RemoteException;
+    void ElectInform(int electiontype) throws RemoteException;
+    RPCProcessInterface getCoordinator() throws RemoteException;
+    boolean TalkToCoordinator() throws RemoteException;
 }
