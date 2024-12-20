e7a90ef038d8b27150d4ebf8442efbed8e7b1aa4
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2024-12-20 22:48:37 +0530

    added common list and while true (main) function

--- 19Z611 - Distributed Computing Laboratory/Experiment 3/RPC Election/RPCClient.java
+++ 19Z611 - Distributed Computing Laboratory/Experiment 3/RPC Election/RPCClient.java
@@ -1,6 +1,7 @@
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
-
+import java.util.ArrayList;
+import java.util.List;
 public class RPCClient {
     public static void main(String[] args) {
         try {
@@ -12,10 +13,39 @@ public class RPCClient {
 
             Registry registry3 = LocateRegistry.getRegistry("localhost", 4003);
             RPCProcessInterface process3 = (RPCProcessInterface) registry3.lookup("process3");
+            
+            List<RPCProcessInterface> AllProcessesList = new ArrayList<RPCProcessInterface>();
+            AllProcessesList.add(process1);
+            AllProcessesList.add(process2);
+            AllProcessesList.add(process3);
+            
+            System.out.println("Process 1 priority: " + process1.getProcesses());
+            System.out.println("Process 2 priority: " + process2.getProcesses());
+            System.out.println("Process 3 priority: " + process3.getProcesses());
 
-            System.out.println("Process 1 priority: " + process1.getPriority());
-            System.out.println("Process 2 priority: " + process2.getPriority());
-            System.out.println("Process 3 priority: " + process3.getPriority());
+            while (true) {
+                process1.getCoordinator().randomlyKillYourself();
+                boolean ElectionNeeded = false;
+                int WakeupPriority;
+                RPCProcessInterface WhistleBlower = process1;             
+                for (RPCProcessInterface process : AllProcessesList) {
+                    WakeupPriority = process.reducetimeout();
+                    if (!process.TalkToCoordinator() || WakeupPriority > process.getCoordinator().getPriority() ) {
+                        ElectionNeeded = true;
+                        WhistleBlower = process;
+                    }
+                }
+
+                if (ElectionNeeded) {
+                    WhistleBlower.ElectInform(1);
+                }
+
+                try {
+                    Thread.sleep(1000); // Delay for 1 second
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
         } catch (Exception e) {
             e.printStackTrace();
     }
