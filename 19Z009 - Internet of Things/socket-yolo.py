2a476bed484c8ca1dfa0ac1affcbff0dd28e3b19
Akash Shanmugaraj <akashshanmugaraj@gmail.com>
2025-05-02 19:26:13 +0530

    final commit for 19Z009

--- 19Z009 - Internet of Things/socket-yolo.py
+++ 19Z009 - Internet of Things/socket-yolo.py
@@ -32,7 +32,7 @@ except Exception as e:
     exit()
 
 # IP Camera stream (Update with actual IP)
-camera_url = "http://172.20.10.5:4747/mjpegfeed"
+camera_url = "http://100.114.252.62:4747/mjpegfeed"
 cap = cv2.VideoCapture(camera_url, cv2.CAP_FFMPEG)
 
 if not cap.isOpened():
